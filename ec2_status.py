8cfa38a93adccf398c63582b03c39abc9b8748a7
Micheal Kingston <micheal.kingston@outlook.com>
2023-06-20 11:57:09 +0100

    completed ec2 check script

--- ec2_status.py
+++ ec2_status.py
@@ -1,15 +1,26 @@
 import boto3
+import schedule
 
 ec2_client = boto3.client('ec2', region_name="eu-west-1")
 ec2_resource = boto3.resource('ec2', region_name="eu-west-1")
 
-#use describe_instance_status function for more details
 
-ec2_statuses = ec2_client.describe_instance_status()
+def check_instance_status():
+    ec2_statuses = ec2_client.describe_instance_status(
+        IncludeAllInstances=True
+    )
 
-for status in ec2_statuses['InstanceStatuses']:
-    ins_status = status['InstanceStatus']['Status']
-    sys_status = status['SystemStatus']['Status']
-    state = status['InstanceState']['Name']
+    for status in ec2_statuses['InstanceStatuses']:
+        ins_status = status['InstanceStatus']['Status']
+        sys_status = status['SystemStatus']['Status']
+        state = status['InstanceState']['Name']
 
-    print(f"Instance {status['InstanceId']} status is {state}, with instance status {ins_status} and system status {sys_status}")
+        print(f"Instance {status['InstanceId']} is {state}, with instance status {ins_status} and system status {sys_status}.")
+    print("#################################\n")
+
+
+schedule.every(5).seconds.do(check_instance_status)
+schedule.every().friday.at("12:00")
+
+while True:
+    schedule.run_pending()
